7c23317ac00692eb266b7e80256239448d1bae65
ClementSellyPro <clementselly.pro@gmail.com>
2025-08-02 15:02:24 +0400

    Little correction to save properly tags. Return color instead of name in TagDTO

--- src/main/java/com/nextask/nextask_app/api/DTO/TagDTO.java
+++ src/main/java/com/nextask/nextask_app/api/DTO/TagDTO.java
@@ -19,6 +19,6 @@ public class TagDTO {
   public void setId(String id) { this.id = id; }
   public String getName() { return name; }
   public void setName(String name) { this.name = name; }
-  public String getColor() { return name; }
+  public String getColor() { return color; }
   public void setColor(String color) { this.color = color; }
  }
